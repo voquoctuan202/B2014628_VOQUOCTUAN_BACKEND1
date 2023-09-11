e8965df7d6db3868ed9b79a87402f106bd3afdde
voquoctuan202 <voquoctuan425@gmail.com>
2023-09-11 15:38:08 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const ApiError = require("./app/api-error")
+const contactsRouter = require("./app/routes/contact.route")
 const cors = require("cors")
 
 const app = express()
@@ -9,5 +11,17 @@ app.use(express.json())
 app.get("/",(req,res)=>{
     res.json({ message: "Welcome to contact book application." })
 })
+app.use("/api/contact", contactsRouter)
+
+app.use((req,res,next)=>{
+    return next(new ApiError(404, "Resource not found"))
+})
+
+app.use((err, req, res, next)=>{
+    return res.status(ApiError.statusCode || 500).json({
+        message : ApiError.message || "Internal Server Error"
+    })
+})
+
 
 module.exports = app
